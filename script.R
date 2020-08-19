3ae0698b2d98291b2a6d24e8a30c7f2d4600d7df
saboye <43153333+saboye@users.noreply.github.com>
2020-08-19 02:28:26 -0500

    Adding the ggplot

--- script.R
+++ script.R
@@ -34,3 +34,49 @@ gapminder %>%
 # Use mutate to create a new column called lifeExpMonths
 gapminder %>%
   mutate(lifeExpMonths =lifeExp * 12)
+
+# Filter, mutate, and arrange the gapminder dataset
+gapminder %>%
+  filter(year ==2007) %>%
+  mutate(lifeExpMonths = lifeExp *12) %>%
+  arrange(desc(lifeExpMonths))
+
+# Create gapminder_1952
+gapminder_1952 <- gapminder %>%
+  filter(year ==1952)
+
+# Change to put pop on the x-axis and gdpPercap on the y-axis
+ggplot(gapminder_1952, aes(x =pop , y = gdpPercap)) +
+  geom_point()
+
+# Create a scatter plot with pop on the x-axis and lifeExp on the y-axis
+ggplot(gapminder_1952, aes(x=pop, y=lifeExp))+
+  geom_point()
+
+
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
+  geom_point()
+  scale_x_log10()
+  
+  
+# Scatter plot comparing pop and gdpPercap, with both axes on a log scale
+  ggplot(gapminder_1952, aes(x=pop, y=gdpPercap))+
+    geom_point()
+    scale_x_log10()
+    scale_y_log10()
+    
+# Add the size aesthetic to represent a country's gdpPercap
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent, size= gdpPercap)) +
+  geom_point() +
+  scale_x_log10()    
+
+# Scatter plot comparing gdpPercap and lifeExp, with color representing continent
+# and size representing population, faceted by year
+ggplot(gapminder, aes(x=gdpPercap,y=lifeExp, color=continent, size=pop))+ 
+  geom_point()+
+  scale_x_log10()+
+  facet_wrap(~ year)
+
+
+    
+    
